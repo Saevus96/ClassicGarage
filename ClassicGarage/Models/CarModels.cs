9a92af2835ed5a7c0a61db46bf8a442c2854693e
Saevus96 <kp.chlewski@gmail.com>
2019-01-11 08:55:05 +0100

    Dodaj pliki projektów.

---
+++ ClassicGarage/Models/CarModels.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClassicGarage.Models
+{
+    public class CarModels
+    {
+        public int ID { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int VIN { get; set; }
+        public string Name { get; set; }
+        public string Picture { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public double PurchaseAmount { get; set; }
+        public DateTime SalesDate { get; set; }
+        public double SalesAmount { get; set; }
+        public int OwnerID { get; set; }
+
+
+        public virtual OwnerModels Owner { get; set; }
+
+        public virtual ICollection<RepairsModels> Repair { get; set; }
+
+        public virtual ICollection<PartsModels> Part { get; set; }
+
+        
+
+
+    }
+}
